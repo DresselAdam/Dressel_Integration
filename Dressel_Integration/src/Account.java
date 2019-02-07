bd55b3e32efbbda8e8bdffab76500b5878902904
Adam Dressel <dresseladam3@users.noreply.github.com>
2019-02-07 16:03:27 -0500

    Made main even more concise and created a new method.

--- Dressel_Integration/src/Account.java
+++ Dressel_Integration/src/Account.java
@@ -3,19 +3,48 @@ import java.util.Scanner;
 
 public class Account {
   private double balance;
-  private Scanner scan = new Scanner(System.in);
+  private static Scanner scan = new Scanner(System.in);
+  private static String acctMenu =
+      "Choose an account - Enter 1 for Checking " + "- Enter 2 for Savings";
+  private static int acctNum;
 
-  // Withdrawal method, main class takes user input and subtracts it
-  // from the balance.
+  // setBalance method uses random to generate a random balance for an account.
   public void setBalance() {
     balance = (Math.random() * 1000);
   }
 
+  // getBalance returns the value of balance, first created by setBalance,
+  // and then altered by the various other methods.
   public double getBalance() {
     return balance;
   }
+  // The acctMenu method option is used to choose an account inside each menu
+  // option in main. For example if the user chooses withdrawal, the main
+  // method will run the acctMenu method, let the user decide on an account,
+  // and then run the withdrawal for that account. The acctMenu is static
+  // because we are using it to choose a specific class object.
 
+  public static Account acctMenu(Account acctNum1, Account acctNum2) {
+    Account chosenAcct = new Account();
+    do {
+      System.out.println(acctMenu);
+      acctNum = scan.nextInt();
+      if (acctNum == 1) {
+        chosenAcct = acctNum1;
+      } else if (acctNum == 2) {
+        chosenAcct = acctNum2;
+      } else {
+        System.out.println("Error, please choose a valid option.");
+      }
+    } while (acctNum != 1 && acctNum != 2);
+    return chosenAcct;
+  }
+
+  // The withdrawal method is used by subtracting a given amount from the
+  // object's current balance. An if statement is used in case a user attempts
+  // to withdraw more money than they have in their account balance.
   public double withdrawal() {
+    System.out.println("Choose amount to withdraw:");
     int withAmnt = scan.nextInt();
     if (withAmnt > balance) {
       System.out.println("Not enough funds.");
@@ -25,6 +54,8 @@ public class Account {
     return balance;
   }
 
+  // The deposit method works similar to the withdraw, except we are adding to
+  // the balance of the chosen account.
   public double deposit() {
     int depAmnt = scan.nextInt();
     balance += depAmnt;
--- Dressel_Integration/src/Main.java
+++ Dressel_Integration/src/Main.java
@@ -5,113 +5,33 @@ import java.text.DecimalFormat;
 // Adam Dressel
 // Basic ATM machine simulator. Integrating basic skills learned about java.
 
-/*
- * byte: The byte data type is an 8-bit signed two's complement integer. It has a minimum value of
- * -128 and a maximum value of 127 (inclusive). The byte data type can be useful for saving memory
- * in large arrays, where the memory savings actually matters. They can also be used in place of int
- * where their limits help to clarify your code; the fact that a variable's range is limited can
- * serve as a form of documentation.
- * 
- * short: The short data type is a 16-bit signed two's complement integer. It has a minimum value of
- * -32,768 and a maximum value of 32,767 (inclusive). As with byte, the same guidelines apply: you
- * can use a short to save memory in large arrays, in situations where the memory savings actually
- * matters.
- * 
- * int: By default, the int data type is a 32-bit signed two's complement integer, which has a
- * minimum value of -231 and a maximum value of 231-1. In Java SE 8 and later, you can use the int
- * data type to represent an unsigned 32-bit integer, which has a minimum value of 0 and a maximum
- * value of 232-1. Use the Integer class to use int data type as an unsigned integer. See the
- * section The Number Classes for more information. Static methods like compareUnsigned,
- * divideUnsigned etc have been added to the Integer class to support the arithmetic operations for
- * unsigned integers.
- * 
- * long: The long data type is a 64-bit two's complement integer. The signed long has a minimum
- * value of -263 and a maximum value of 263-1. In Java SE 8 and later, you can use the long data
- * type to represent an unsigned 64-bit long, which has a minimum value of 0 and a maximum value of
- * 264-1. Use this data type when you need a range of values wider than those provided by int. The
- * Long class also contains methods like compareUnsigned, divideUnsigned etc to support arithmetic
- * operations for unsigned long.
- * 
- * float: The float data type is a single-precision 32-bit IEEE 754 floating point. Its range of
- * values is beyond the scope of this discussion, but is specified in the Floating-Point Types,
- * Formats, and Values section of the Java Language Specification. As with the recommendations for
- * byte and short, use a float (instead of double) if you need to save memory in large arrays of
- * floating point numbers. This data type should never be used for precise values, such as currency.
- * For that, you will need to use the java.math.BigDecimal class instead. Numbers and Strings covers
- * BigDecimal and other useful classes provided by the Java platform.
- * 
- * double: The double data type is a double-precision 64-bit IEEE 754 floating point. Its range of
- * values is beyond the scope of this discussion, but is specified in the Floating-Point Types,
- * Formats, and Values section of the Java Language Specification. For decimal values, this data
- * type is generally the default choice. As mentioned above, this data type should never be used for
- * precise values, such as currency.
- * 
- * boolean: The boolean data type has only two possible values: true and false. Use this data type
- * for simple flags that track true/false conditions. This data type represents one bit of
- * information, but its "size" isn't something that's precisely defined.
- * 
- * char: The char data type is a single 16-bit Unicode character. It has a minimum value of '\u0000'
- * (or 0) and a maximum value of '\uffff' (or 65,535 inclusive).
- * 
- * source: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
- */
 
-// Variables - a location in memory
-// Scope - A variable is only accessible in the method it was declared.
-
-// Input from numbers to strings - must clear buffer.
-
-
-/*
- * Whenever you divide integers, the result is an integer, and any floating point number gets
- * truncated.
- */
 public class Main {
 
   public static void main(String[] args) {
     // New scanner object reads user input
     Scanner scan = new Scanner(System.in);
-<<<<<<< HEAD
     System.out.println("Welcome to to Bank of Java termninal!");
     DecimalFormat money = new DecimalFormat(".##");
-=======
-    System.out.println("Welcome to to Bank of Java terminal!");
->>>>>>> branch 'master' of https://github.com/Slimes3/Dressel_Integration
     // Random balance generated for each account. And stored as double balance
     Account checking = new Account();
     checking.setBalance();
     Account savings = new Account();
     savings.setBalance();
 
-    /*
-     * final keyword used for a fee applied to a withdrawal transaction. The final keyword is used
-     * for a constant, unchanging variable. This helps me avoid making errors when applying the fee
-     * to calculations.
-     */
-    /*
-     * Casting variables is used to convert variable types. This is useful performing operations
-     * with two different variable types, such as an int and a double.
-     */
+    // final keyword used for a fee applied to a withdrawal transaction. The final keyword is used
+    // for a constant, unchanging variable. This helps me avoid making errors when applying the fee
+    // to calculations.
     final int FEE = (int) 1;
 
-<<<<<<< HEAD
-    String accountMenu = "Choose an account - Enter 1 for Checking " + "- Enter 2 for Savings";
+    String menu = " - Enter 1 for Withdrawal \n " + "- Enter 2 for Deposit \n "
+        + "- Enter 3 to Check Balances \n " + "- Enter 4 to Exit";
 
-    String menu = "Enter 1 for Withdrawal - Enter 2 for Deposit - " + "Enter 3 to Check Balances";
-=======
-    String menu = "Enter 1 for Withdrawal \n - Enter 2 for Deposit \n - Enter 3 to Check Balances";
->>>>>>> branch 'master' of https://github.com/Slimes3/Dressel_Integration
 
     // keepLoop boolean to determine whether to continue the program.
+    // only changed to false when the user chooses the exit option.
+    boolean keepLoop = true;
 
-    boolean keepLoop = false;
-    /*
-     * A do/while loop is used so that the messages will replay if the user enters an invalid option
-     * or input. At the end of each branch the keepLoop value is set to the corresponding value.
-     */
-    /*
-     * User enters branch where they can either deposit or withdrawal money from current balance.
-     */
     do {
       System.out.println(menu);
       int userChoice = scan.nextInt();
@@ -120,48 +40,23 @@ public class Main {
       // account.
 
       if (userChoice == 1) {
-        System.out.println(accountMenu);
-        int acctNum = scan.nextInt();
-        if (acctNum == 1) {
-          System.out.println(checking.withdrawal());
-        } else if (acctNum == 2) {
-          System.out.println(savings.withdrawal());
-        }
-        keepLoop = true;
-
+        System.out.println(Account.acctMenu(checking, savings).withdrawal());
       }
       // Second option, simply a deposit option.
       // User can currently deposit any integer they choose.
       // Similar to withdrawal option, calls the deposit method on the chosen
       // account.
       else if (userChoice == 2) {
-        keepLoop = true;
-        System.out.println(accountMenu);
-        int acctNum = scan.nextInt();
-        if (acctNum == 1) {
-          System.out.println(checking.deposit());
-        } else if (acctNum == 2) {
-          System.out.println(savings.deposit());
-        }
+        System.out.println(Account.acctMenu(checking, savings).deposit());
       }
 
       // Third option, displays indicated balance by calling the getBalance()
       // method on the chosen account.
       else if (userChoice == 3) {
-        System.out.println(accountMenu);
-        int accountNum = scan.nextInt();
-        if (accountNum == 1) {
-          System.out.println(money.format(checking.getBalance()) + "$");
-        } else if (accountNum == 2) {
-          System.out.println(money.format(savings.getBalance()) + "$");
-        } else {
-
-        }
-        keepLoop = true;
-        //Exit menu option, turns the keepLoop boolean variable to false, this
+        System.out.println(Account.acctMenu(checking, savings).getBalance());
+        // Exit menu option, turns the keepLoop boolean variable to false, this
         // exit the loop and close the scanner.
-      }
-      else if(userChoice == 4) {
+      } else if (userChoice == 4) {
         keepLoop = false;
       }
       // All other integers, result in repeating the loop.
